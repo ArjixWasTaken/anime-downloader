4119ef448ab4f6d6fbba4ed0d34fe8b6c3f8c089
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-12-14 13:34:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'requests-cache>=0.4.13',
         'tabulate>=0.8.3',
         'pycryptodome>=3.8.2',
+        'youtube-dl'
     ],
     extras_require={
         'selescrape': ['selenium'],
